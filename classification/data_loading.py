8a6514964f8267c2f25040dfa06cbeff854d7faf
ge52wew <max.pittner@tum.de>
2020-03-02 15:18:05 +0100

    add dataloader and changed rnn

---
+++ classification/data_loading.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from e_nose import file_reader
+from e_nose import data_processing as dp
+
+
+def get_measurements_from_dir(directory_name='../data'):
+    functionalisations, correct_channels, data = file_reader.read_all_files_in_folder(directory_name)
+    measurements_per_file = {}
+    for file in data:
+        measurements_per_file[file] = dp.get_labeled_measurements(data[file], correct_channels, functionalisations)
+
+    measurements = []
+    for file in measurements_per_file:
+        adding = dp.standardize_measurements(measurements_per_file[file])
+        if adding is not None:
+            measurements.extend(adding)
+
+    return np.array(measurements)
+
+def train_test_split(measurements, split=0.8):
+    labels_measurements = [m.label for m in measurements]
+    labels = np.unique(labels_measurements)
+
+    for i, l in enumerate(labels):
+        indices_label = np.argwhere(np.array(labels_measurements) == l).flatten()
+
+        num_samples = indices_label.size()
+        if i == 0:
+            measurements_train = measurements[indices_label][:int(split*num_samples)]
+            measurements_test = measurements[indices_label][int(split*num_samples):]
+        else:
+            measurements_train = np.append(measurements_train, measurements[indices_label][:int(split*num_samples)])
+            measurements_test = np.append(measurements_test, measurements[indices_label][int(split*num_samples):])
+
+    np.random.shuffle(measurements_train)
+    np.random.shuffle(measurements_test)
+
+    return measurements_train, measurements_test
+
+def shuffle(dataset_one, dataset_two=None):
+    np.random.shuffle(dataset_one)
+    if dataset_two is not None:
+        np.random.shuffle(dataset_two)
+        return dataset_one, dataset_two
+    else:
+        return dataset_one
+
--- classification/rnn.py
+++ classification/rnn.py
@@ -1,12 +1,12 @@
 import numpy as np
 import tensorflow as tf
+import random
 
-from e_nose import file_reader
-from e_nose import data_processing as dp
+from classification.data_loading import get_measurements_from_dir, train_test_split, shuffle
 
 
-def get_batched_data(measurements, data_batch, masking_value, batch_size=3, sequence_length=3):
-    dim = 64
+def get_batched_data(measurements, masking_value, batch_size=3, sequence_length=3, dimension=64):
+
     measurement_indices = np.arange(len(measurements))
     np.random.shuffle(measurement_indices)
 
@@ -31,7 +31,7 @@ def get_batched_data(measurements, data_batch, masking_value, batch_size=3, sequ
                 #print(classes_dict[measurements[index].label])
                 series_labels = np.ones(shape=(series_data.shape[0], 1), dtype=int) * classes_dict[measurements[index].label]
             else:
-                series_data = np.ones(shape=(1, dim), dtype=float) * masking_value
+                series_data = np.ones(shape=(1, dimension), dtype=float) * masking_value
                 series_labels = np.ones(shape=(1, 1), dtype=int) * masking_value
 
             if series_data.shape[0] > max_len:
@@ -39,7 +39,7 @@ def get_batched_data(measurements, data_batch, masking_value, batch_size=3, sequ
             batch_list.append(series_data)
             batch_list_labels.append(series_labels)
 
-        batch = np.ones(shape=(batch_size, max_len, dim), dtype=float) * masking_value
+        batch = np.ones(shape=(batch_size, max_len, dimension), dtype=float) * masking_value
         batch_labels = np.ones(shape=(batch_size, max_len, 1), dtype=int) * masking_value
 
         for b in range(batch_size):
@@ -53,7 +53,7 @@ def get_batched_data(measurements, data_batch, masking_value, batch_size=3, sequ
         ba_labels = batches_labels[i]
         padding_length = sequence_length - (ba.shape[1] % sequence_length)
         if padding_length != sequence_length:
-            ba = np.append(ba, np.ones(shape=(batch_size, padding_length, dim), dtype=float) * masking_value, axis=1)
+            ba = np.append(ba, np.ones(shape=(batch_size, padding_length, dimension), dtype=float) * masking_value, axis=1)
             ba_labels = np.append(batches_labels[i], np.ones(shape=(batch_size, padding_length, 1), dtype=int) * masking_value, axis=1)
         split = int(ba.shape[1] / sequence_length)
 
@@ -73,26 +73,9 @@ def get_batched_data(measurements, data_batch, masking_value, batch_size=3, sequ
     print(type(batches_labels_done))
     print(batches_labels_done)
 
-    return batches_data_done, batches_labels_done, starting_indices
+    dataset = tf.data.Dataset.from_tensor_slices((tf.constant(batches_data), tf.constant(batches_labels)))
 
-
-def get_measurements():
-    functionalisations, correct_channels, data = file_reader.read_all_files_in_folder('../data')
-    measurements_per_file = {}
-    for file in data:
-        print(file)
-        measurements_per_file[file] = dp.get_labeled_measurements(data[file], correct_channels, functionalisations)
-        print(len(measurements_per_file[file]))
-
-    measurements = []
-    for file in measurements_per_file:
-        print("file: ", file)
-        adding = dp.standardize_measurements(measurements_per_file[file])
-        if adding is not None:
-            measurements.extend(adding)
-
-    return np.array(measurements)
-
+    return dataset, starting_indices
 
 def make_model(input_shape, dim_hidden, num_classes, masking_value):
     model = tf.keras.models.Sequential()
@@ -103,39 +86,14 @@ def make_model(input_shape, dim_hidden, num_classes, masking_value):
 
     return model
 
-
 def make_classes_dict(classes):
     classes_dict = {}
     for i, c in enumerate(classes):
         classes_dict[c] = i
     return classes_dict
 
-
 def get_class(c, class_dict):
     return list(class_dict.keys())[list(class_dict.values()).index(c)]
-
-
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-masking_value = 10.
-learning_rate = 0.0005
-dim_hidden = 16
-
-measurements = get_measurements()
-
-classes_list = np.unique([m.label for m in measurements])
-classes_dict = make_classes_dict(classes_list)
-
-
-input_shape = batches_data.shape[1:]
-
-print(batches_data.shape, batches_labels.shape)
-
-print(classes_list)
-print(classes_dict)
-print(classes_dict['cream_cheese'], get_class(5, classes_dict))
-
-lstm = make_model(input_shape, dim_hidden, classes_list.size, masking_value)
 
 def loss(model, X, y, training=False):
     y_pred = model(X, training=training)
@@ -146,28 +104,65 @@ def grad(model, X, y):
         loss_value = loss(model, X, y, training=True)
     return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
+
+batch_size = 1
+sequence_length = 1
+dim = 64
+
+masking_value = 10.
+learning_rate = 0.0005
+dim_hidden = 16
+
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-print(batches_data.shape)
+measurements = get_measurements_from_dir()
+measurements_train, measurements_val = train_test_split(measurements)
+
+classes_list = np.unique([m.label for m in measurements])
+classes_dict = make_classes_dict(classes_list)
+
+
+input_shape = (batch_size, sequence_length, dim)
+
+print(classes_list)
+print(classes_dict)
+#print(classes_dict['cream_cheese'], get_class(5, classes_dict))
+
+lstm = make_model(input_shape, dim_hidden, classes_list.size, masking_value)
+
 num_epochs = 1
-
-dataset = tf.data.Dataset.from_tensor_slices((tf.constant(batches_data), tf.constant(batches_labels)))
-
 
 
 steps = 0
 
 for e in range(num_epochs):
-    counter = 0
-    batches_data, batches_labels, starting_indices = get_batched_data(measurements, True, masking_value, batch_size=1,
-                                                                      sequence_length=1)
 
-    for X, y, in dataset:
-        if counter in starting_indices:
+    measurements_train = shuffle(measurements_train)
+    data_train, starting_indices_train = get_batched_data(measurements_train, masking_value, batch_size=batch_size,
+                                                          sequence_length=sequence_length, dimension=dim)
+
+    counter = 0
+
+    for X, y, in data_train:
+        if counter in starting_indices_train:
             lstm.reset_states()
-        print(np.max(X), np.min(X))
+
         l, grads = grad(lstm, X, y)
         optimizer.apply_gradients(zip(grads, lstm.trainable_variables))
         counter += 1
         steps += 1
         print(counter)
+
+    measurements_val = shuffle(measurements_val)
+    data_val, starting_indices_val = get_batched_data(measurements_val, masking_value, batch_size=batch_size,
+                                                      sequence_length=sequence_length, dimension=dim)
+    counter_val = 0
+
+    for X, y in data_val:
+        if counter_val in starting_indices_val:
+            lstm.reset_states()
+
+        loss(lstm, X, y, training=False)
+        counter_val +=1
+
