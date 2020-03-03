d92b005f2cfa5b2500d7fc093d406acf4dc46368
vstarlinger <valentin@starlinger.net>
2020-03-03 10:28:07 +0100

    added test

--- classification/train_cnn1d_testtf.py
+++ classification/train_cnn1d_testtf.py
@@ -35,151 +35,122 @@ def load_data():
     return train_batch, val_batch
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--smoke-test", action="store_true", help="Finish quickly for testing")
-args, _ = parser.parse_known_args()
+import tensorflow as tf
+
+## CONFIG
+batch_size = 900
+
+## LOAD DATA
+train_batch, val_batch = load_data()
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_batch, train_batch)).batch(batch_size)
+val_ds = tf.data.Dataset.from_tensor_slices((val_batch, val_batch)).batch(batch_size)
+
+####################
+# MODEL SETUP
+####################
+class RecurrentLayer(tf.keras.layers.Layer):
+    def __init__(self, dilation_rate=1, filter_size=64):
+        super(RecurrentLayer, self).__init__()
+        self.sigm_out = tf.keras.layers.Conv1D(filter_size, 2, dilation_rate=2 ** dilation_rate, padding='causal', activation='sigmoid')
+        self.tanh_out = tf.keras.layers.Conv1D(filter_size, 2, dilation_rate=2 ** dilation_rate, padding='causal', activation='tanh')
+        self.same_out = tf.keras.layers.Conv1D(filter_size, 1, padding='same')
+    def call(self, x):
+        original_x = x
+
+        x_t = self.tanh_out(x)
+        x_s = self.sigm_out(x)
+
+        x = tf.keras.layers.Multiply()([x_t, x_s])
+        x = self.same_out(x)
+        x_skips = x
+        x = tf.keras.layers.Add()([original_x, x])
+
+        return x_skips, x
+
+class Model1DCNN(tf.keras.Model):
+    def __init__(self, dilations=3, filter_size=64, input_shape=(64, 49)):
+        super(Model1DCNN, self).__init__()
+
+        self.residual = []
+        self.dilations = dilations
+        self.causal = tf.keras.layers.Conv1D(filter_size, 2, padding='causal', input_shape=input_shape)
+
+        for i in range(1, dilations + 1):
+            self.residual.append(RecurrentLayer(dilation_rate=i, filter_size=filter_size))
+
+        self.same_out_1 = tf.keras.layers.Conv1D(filter_size, 1, padding='same', activation='relu')
+        self.same_out_2 = tf.keras.layers.Conv1D(8, 1, padding='same', activation='relu')
+
+        self.d1 = tf.keras.layers.Dense(400, activation='relu')
+        self.d2 = tf.keras.layers.Dense(200, activation='relu')
+        self.d3 = tf.keras.layers.Dense(20)
+
+    def call(self, x):
+        x_skips = []
+
+        x = self.causal(x)
+        for i in range(self.dilations):
+            x_skip, x = self.residual[i](x)
+            x_skips.append(x_skip)
+
+        x = tf.keras.layers.Add()(x_skips)
+        x = self.same_out_1(x)
+        x = self.same_out_2(x)
+        x = tf.keras.layers.Flatten()(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        return self.d3(x)
+
+####################
+# LOSS DEFINITION
+####################
+def triplet_loss(feats):
+    m = 0.01
+
+    diff_pos = feats[0:batch_size:3] - feats[1:batch_size:3]
+    diff_pos_sq = tf.math.reduce_sum(diff_pos ** 2, axis=1)
+    diff_neg = feats[0:batch_size:3] - feats[2:batch_size:3]
+    diff_neg_sq = tf.math.reduce_sum(diff_neg ** 2, axis=1)
+
+    L_triplet = tf.math.reduce_sum(tf.math.maximum(0.0, 1 - diff_neg_sq / (diff_pos_sq + m)))
+
+    L_pair = tf.math.reduce_sum(diff_pos_sq)
+    return L_triplet + L_pair
 
 
-class CNNTrainable(tune.Trainable):
-    def _setup(self, config):
-        import tensorflow as tf
+model = Model1DCNN(dilations=3, filter_size=64)
+optimizer = tf.keras.optimizers.Adam()
 
-        ## CONFIG
-        batch_size = 900
+train_loss = tf.keras.metrics.Mean(name="train_loss")
+val_loss = tf.keras.metrics.Mean(name="val_loss")
 
-        ## LOAD DATA
-        train_batch, val_batch = load_data()
+@tf.function
+def train_step(batch):
+    with tf.GradientTape() as tape:
+        predictions = model(batch)
+        loss = triplet_loss(predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(
+        zip(gradients, model.trainable_variables))
 
-        self.train_ds = tf.data.Dataset.from_tensor_slices((train_batch, train_batch)).batch(batch_size)
-        self.val_ds = tf.data.Dataset.from_tensor_slices((val_batch, val_batch)).batch(batch_size)
+    train_loss(loss)
+
+@tf.function
+def val_step(batch):
+    predictions = model(batch)
+    loss = triplet_loss(predictions)
+
+    val_loss(loss)
 
 
-        ####################
-        # MODEL SETUP
-        ####################
-        class RecurrentLayer(tf.keras.layers.Layer):
-            def __init__(self, dilation_rate=1, filter_size=64):
-                super(RecurrentLayer, self).__init__()
-                self.sigm_out = tf.keras.layers.Conv1D(filter_size, 2, dilation_rate=2 ** dilation_rate, padding='causal', activation='sigmoid')
-                self.tanh_out = tf.keras.layers.Conv1D(filter_size, 2, dilation_rate=2 ** dilation_rate, padding='causal', activation='tanh')
-                self.same_out = tf.keras.layers.Conv1D(filter_size, 1, padding='same')
-            def call(self, x):
-                original_x = x
+for idx, (batch, _) in enumerate(train_ds):
+    print("train",batch.shape)
+    train_step(batch)
 
-                x_t = self.tanh_out(x)
-                x_s = self.sigm_out(x)
+for batch, _ in val_ds:
+    print("val",batch.shape)
+    val_step(batch)
 
-                x = tf.keras.layers.Multiply()([x_t, x_s])
-                x = self.same_out(x)
-                x_skips = x
-                x = tf.keras.layers.Add()([original_x, x])
-
-                return x_skips, x
-
-        class Model1DCNN(tf.keras.Model):
-            def __init__(self, dilations=3, filter_size=64, input_shape=(64, 49)):
-                super(Model1DCNN, self).__init__()
-
-                self.residual = []
-                self.dilations = dilations
-                self.causal = tf.keras.layers.Conv1D(filter_size, 2, padding='causal', input_shape=input_shape)
-
-                for i in range(1, dilations + 1):
-                    self.residual.append(RecurrentLayer(dilation_rate=i, filter_size=filter_size))
-
-                self.same_out_1 = tf.keras.layers.Conv1D(filter_size, 1, padding='same', activation='relu')
-                self.same_out_2 = tf.keras.layers.Conv1D(8, 1, padding='same', activation='relu')
-
-                self.d1 = tf.keras.layers.Dense(400, activation='relu')
-                self.d2 = tf.keras.layers.Dense(200, activation='relu')
-                self.d3 = tf.keras.layers.Dense(20)
-
-            def call(self, x):
-                x_skips = []
-
-                x = self.causal(x)
-                for i in range(self.dilations):
-                    x_skip, x = self.residual[i](x)
-                    x_skips.append(x_skip)
-
-                x = tf.keras.layers.Add()(x_skips)
-                x = self.same_out_1(x)
-                x = self.same_out_2(x)
-                x = tf.keras.layers.Flatten()(x)
-                x = self.d1(x)
-                x = self.d2(x)
-                return self.d3(x)
-
-        ####################
-        # LOSS DEFINITION
-        ####################
-        def triplet_loss(self, feats):
-            m = 0.01
-
-            diff_pos = feats[0:batch_size:3] - feats[1:batch_size:3]
-            diff_pos_sq = tf.math.reduce_sum(diff_pos ** 2, axis=1)
-            diff_neg = feats[0:batch_size:3] - feats[2:batch_size:3]
-            diff_neg_sq = tf.math.reduce_sum(diff_neg ** 2, axis=1)
-
-            L_triplet = tf.math.reduce_sum(tf.math.maximum(0.0, 1 - diff_neg_sq / (diff_pos_sq + m)))
-
-            L_pair = tf.math.reduce_sum(diff_pos_sq)
-            return L_triplet + L_pair
-
-        self.model = Model1DCNN(dilations=config["num_dilations"], filter_size=config["filter_size"])
-
-        self.train_loss = tf.keras.metrics.Mean(name="train_loss")
-        self.val_loss = tf.keras.metrics.Mean(name="val_loss")
-
-        @tf.function
-        def train_step(batch):
-            with tf.GradientTape() as tape:
-                predictions = self.model(batch)
-                loss = triplet_loss(predictions)
-            gradients = tape.gradient(loss, self.model.trainable_variables)
-            self.optimizer.apply_gradients(
-                zip(gradients, self.model.trainable_variables))
-
-            self.train_loss(loss)
-
-        @tf.function
-        def val_step(batch):
-            predictions = self.model(batch)
-            loss = triplet_loss(predictions)
-
-            self.val_loss(loss)
-
-        self.tf_train_step = train_step
-        self.tf_val_step = val_step
-
-    def _train(self):
-        self.train_loss.reset_states()
-        self.val_loss.reset_states()
-
-        for idx, (images, labels) in enumerate(self.train_ds):
-            self.tf_train_step(images, labels)
-
-        for test_images, test_labels in self.test_ds:
-            self.tf_test_step(test_images, test_labels)
-
-        # It is important to return tf.Tensors as numpy objects.
-        return {
-            "epoch": self.iteration,
-            "loss": self.train_loss.result().numpy(),
-            "test_loss": self.test_loss.result().numpy(),
-        }
-
-ray.init(num_cpus=2 if args.smoke_test else None)
-tune.run(
-    CNNTrainable,
-    stop={"training_iteration": 5 if args.smoke_test else 50},
-    verbose=1,
-    name="cnn_wavenet_roboy",
-    num_samples=1,
-    config={
-        "lr": tune.sample_from(lambda spec: np.random.uniform(0.0001, 0.001)),
-        "num_dilations": tune.grid_search([3]),
-        "filter_size": tune.grid_search([64, 128])
-    })
-
+print(train_loss)
+print(val_loss)
