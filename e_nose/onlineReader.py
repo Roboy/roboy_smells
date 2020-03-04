76f9ca893b0923f544d4971a83798b1478f03d29
Daniel Schubert <daniel.schubert@tum.de>
2020-03-04 13:44:45 +0100

    Support different standardization types for OnlineReader

--- e_nose/onlineReader.py
+++ e_nose/onlineReader.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from e_nose.file_reader import get_sensor_spec
-from e_nose.measurements import Measurement
+from e_nose.measurements import Measurement, StandardizationType
 
 
 class OnlineReader:
@@ -12,18 +12,26 @@ class OnlineReader:
     l1_factor = 1e-3
     """ Factor for the low-pass filter """
 
-    def __init__(self, sensor_id: int, max_history_length: int = 100_000):
+    def __init__(self, sensor_id: int,
+                 standardization: StandardizationType = StandardizationType.LOWPASS_FILTER,
+                 max_history_length: int = 100_000):
         """
 
         :param sensor_id: ID of the sensor to use => to know the functionalisations and failure bits
+        :param standardization: The kind of standardization to apply to the live data;
+                they may not be entirely correct, though...
         :param max_history_length: Max length of the history data... to avoid using too much memory
         """
         self.data: np.ndarray = np.empty((0, 64))
         self.log_lowpass: np.ndarray = np.empty((0, 64))
         self.log_lowpass_current = None
         self.max_history_length: int = max_history_length
+        self.standardization: StandardizationType = standardization
 
         self.functionalisations, self.working_channels = get_sensor_spec(sensor_id)
+
+    def set_standardization_type(self, standardization: StandardizationType):
+        self.standardization = standardization
 
     def add_sample(self, sample: np.ndarray) -> ():
         """ Adds a single sample to the internal state; updates the internal low-pass automatically """
@@ -55,6 +63,18 @@ class OnlineReader:
                                   self.working_channels, self.functionalisations,
                                   0, 0, 0, 0, 0  # No BME-Data
                                   )
-        measurement.reference_measurement = self.log_lowpass[-n]
+
+        if self.standardization == StandardizationType.BEGINNING_AVG:
+            measurement.set_reference(None, StandardizationType.BEGINNING_AVG)
+
+        elif self.standardization == StandardizationType.LOWPASS_FILTER:
+            measurement.set_reference(self.log_lowpass[-n], StandardizationType.LOWPASS_FILTER)
+
+        elif self.standardization == StandardizationType.LAST_REFERENCE:
+            # We cannot actually perform this standardization as we hav got no idea what the last
+            # reference measurement might have been...
+            # To simulate the same behaviour though, we just set it to the average of the last 10 samples just before
+            reference = np.mean(np.log(self.data[-(n+10):-n, :]), axis=0)
+            measurement.set_reference(reference, StandardizationType.LAST_REFERENCE)
 
         return measurement
