37229a4ca9e87e2c08c605bdefa54b09c8e54642
ge52wew <max.pittner@tum.de>
2020-03-05 19:09:11 +0100

    implemented live inference for SmelLSTM

--- classification/inference_lstm.py
+++ classification/inference_lstm.py
@@ -3,7 +3,7 @@ import numpy as np
 import classification.data_loading as dl
 import os
 
-from classification.lstm_model import make_model
+from classification.lstm_model import make_model, SmelLSTM
 from classification.util import get_classes_list, get_class, get_classes_dict, hot_fix_label_issue
 
 from e_nose.measurements import DataType
@@ -11,22 +11,22 @@ from e_nose.measurements import DataType
 #model_name = "LSTMTrainable_b625122c_11_batch_size=64,dim_hidden=16,lr=0.073956,return_sequences=True_2020-03-04_19-04-41c78mu_or"
 #checkpoint = 150
 
-model_name = 'LSTMTrainable_ae3ce78a_36_batch_size=32,dim_hidden=16,lr=0.0018266,return_sequences=True_2020-03-04_21-40-27rsrdzrqd'
-checkpoint = 90
+model_name = 'LSTMTrainable_15750966_1740_batch_size=128,dim_hidden=6,lr=0.004831,return_sequences=True_2020-03-05_08-08-45fs4p25pg'
+checkpoint = 200
 
 #model_path = '/Users/max/ray_results/lstm_roboy/'
 
-path = './models/rnn/' + model_name + '/checkpoint_' + str(checkpoint) + '/model_weights'
+#path = './models/rnn/' + model_name + '/checkpoint_' + str(checkpoint) + '/model_weights'
 #path = model_path + model_name + '/checkpoint_' + str(checkpoint) + '/model_weights'
 
 batch_size = 1
-sequence_length = 10
+sequence_length = 1
 #dim = 49
 dim = 42
 input_shape = (batch_size, sequence_length, dim)
 
 masking_value = 100.
-dim_hidden = 16
+dim_hidden = 6
 return_sequences = True
 
 data_path = os.path.join(os.getcwd(), '../data_test')
@@ -42,11 +42,13 @@ classes_list_gt = get_classes_list(measurements)
 print(classes_list_gt)
 classes_dict_gt = get_classes_dict(classes_list_gt)
 
-lstm = make_model(input_shape=input_shape, dim_hidden=dim_hidden, num_classes=len(classes_list_pred), masking_value=masking_value, return_sequences=return_sequences)
+lstm = SmelLSTM(input_shape=input_shape, hidden_dim_simple=dim_hidden, num_classes=len(classes_list_pred), masking_value=masking_value, return_sequences=return_sequences)
 lstm.summary()
-lstm.load_weights(path)
 
-#print(classes_dict)
+lstm.load_weights(model_name=model_name, checkpoint=checkpoint, path='./models/rnn/')
+
+for m in measurements:
+    print(lstm.predict_live(m))
 
 '''
 
@@ -57,8 +59,9 @@ label_gt = m.label
 data = m.get_data()
 '''
 
-lower_threshold = 0
-upper_threshold = 1000
+'''
+lower_threshold = 10
+upper_threshold = 70
 counter = 0
 counter_correct = 0
 
@@ -68,7 +71,7 @@ for m in measurements:
     if label_gt not in classes_list_pred or label_gt == 'coffee_powder' or label_gt == 'raisin':
         continue
     print('label: ', label_gt)
-    data = m.get_data_as(DataType.FULL)
+    data = m.get_data_as(DataType.HIGH_PASS)
 
     end = int(data.shape[0] / sequence_length)
     for i in range(end):
@@ -97,3 +100,4 @@ for m in measurements:
 
 accuracy = counter_correct/counter
 print('accuracy:', accuracy)
+'''
--- classification/lstm_model.py
+++ classification/lstm_model.py
@@ -1,3 +1,6 @@
+from e_nose.measurements import DataType
+import numpy as np
+
 def make_model(input_shape, dim_hidden, num_classes, masking_value=100., return_sequences=True):
     from tensorflow import keras
 
@@ -34,3 +37,50 @@ def make_model_deeper(input_shape, num_classes, hidden_dim_1=32, hidden_dim_2=16
         model.add(keras.layers.Dense(num_classes))
 
     return model
+
+
+#def get_model_with_weights(model_name, checkpoint, path='./models/rnn/', input_dimension=42, sequence_length=1, hidden_dim=8, num_classes=6, simple_model=True):
+#    path_to_model = path + model_name + '/checkpoint_' + str(checkpoint) + '/model_weights'
+
+#    model.summary()
+#    model.load_weights(path_to_model)
+#    return model
+
+
+class SmelLSTM:
+    def __init__(self, input_shape, num_classes, masking_value=100., return_sequences=True, simple_model=True,
+                 hidden_dim_simple=6, data_type=DataType.HIGH_PASS, classes_list = ['coffee_powder', 'isopropanol', 'orange_juice', 'raisin', 'red_wine', 'wodka']):
+
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.masking_value = masking_value
+        self.return_sequences = return_sequences
+        self.simple_model = simple_model
+        self.hidden_dim_simple = hidden_dim_simple
+        self.batch_size = input_shape[0]
+        self.sequence_length = input_shape[1]
+        self.dimension = input_shape[2]
+        self.data_type = data_type
+        self.classes_list = classes_list
+
+        if self.simple_model:
+            self.model = make_model(input_shape=self.input_shape, dim_hidden=self.hidden_dim_simple, num_classes=self.num_classes, return_sequences=self.return_sequences)
+        else:
+            self.model = make_model_deeper(input_shape=self.input_shape, num_classes=self.num_classes)
+
+    def load_weights(self, model_name, checkpoint, path='./models/rnn/'):
+        path_to_model = path + model_name + '/checkpoint_' + str(checkpoint) + '/model_weights'
+        self.model.load_weights(path_to_model)
+
+    def predict_live(self, measurement):
+        data = measurement.get_data_as(DataType.HIGH_PASS)
+        self.model.reset_states()
+        sample = np.empty(shape=self.input_shape)
+        for d in range(data.shape[0]):
+            sample[0, 0, :] = data[d, :]
+            y = self.model(sample, training=False)
+        prediction = self.classes_list[np.argmax(y.numpy(), -1).flatten()[0]]
+        return prediction
+
+    def summary(self):
+        self.model.summary()
