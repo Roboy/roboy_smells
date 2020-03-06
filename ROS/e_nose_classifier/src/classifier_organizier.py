e3a2780e9689d074b5d35ab712ec3a45e7381ff0
Wagram Airiian <vagram.airiian@tum.de>
2020-03-06 12:05:25 +0100

    orginizer testing version

--- ROS/e_nose_classifier/src/classifier_organizier.py
+++ ROS/e_nose_classifier/src/classifier_organizier.py
@@ -20,19 +20,23 @@ class ClassifierOrganizer:
                             59, 60, 61, 62]
         working_channels = np.ones(64, bool)
         working_channels[failing_channels] = False
+        num_working_channels = np.count_nonzero(working_channels)
+        print(num_working_channels)
         self.online = OnlineReader(5, override_working_channels=working_channels)
         self.from_sample = 0
         self.sub.onUpdate += self.gotNewSample
         self.online.invoke_callback += self.gatheredData
-        self.classifier = SmelLSTM(input_shape=(1, 1, len(working_channels)), num_classes=6, hidden_dim_simple=6)
+        self.classifier = SmelLSTM(input_shape=(1, 1, num_working_channels), num_classes=6, hidden_dim_simple=6)
 
         #self.model_name = "LSTMTrainable_b625122c_11_batch_size=64,dim_hidden=16,lr=0.073956,return_sequences=True_2020-03-04_19-04-41c78mu_or"
         #self.model_name = 'LSTMTrainable_15750966_1740_batch_size=128,dim_hidden=6,lr=0.004831,return_sequences=True_2020-03-05_08-08-45fs4p25pg'
 
-        self.model_name = 'LSTMTrainable_9c63b3de_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.018191,return_sequences=True_2020-03-05_14-41-14slhxixe7'
+        #self.model_name = 'LSTMTrainable_9c63b3de_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.018191,return_sequences=True_2020-03-05_14-41-14slhxixe7'
+        #self.model_name = 'LSTMTrainable_987a098a_3_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.076584,return_sequences=True_2020-03-05_14-39-01_0r6hi60'
+        self.model_name = 'LSTMTrainable_f9244f1c_1_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.070814,return_sequences=True_2020-03-05_23-30-57teprjxra'
 
-	#self.classifier.summary()
-        #self.classifier.load_weights(self.model_name, checkpoint=200, path='classification/models/rnn/')
+        self.classifier.summary()
+        self.classifier.load_weights(self.model_name, checkpoint=200, path='classification/models/rnn/')
         print('ros e_nose classification node started successfully')
 
     def startMeas(self):
@@ -50,10 +54,8 @@ class ClassifierOrganizer:
     def gatheredData(self):
         print('gathered data')
         data = self.online.get_since_n_as_measurement(self.from_sample)
-        print(data)
         print(data.correct_channels)
         print(data.get_data().shape)
-        print(data.get_data())
         prediction = self.classifier.predict_live(data)
         print('prediction: ', prediction)
         self.pub_test.send_classification(prediction)
