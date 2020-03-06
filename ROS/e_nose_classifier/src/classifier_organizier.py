6252aa6530280a1f18ff6c4d5bb7ba4595b90668
ge52wew <max.pittner@tum.de>
2020-03-07 00:23:06 +0100

    add knn

--- ROS/e_nose_classifier/src/classifier_organizier.py
+++ ROS/e_nose_classifier/src/classifier_organizier.py
@@ -3,6 +3,8 @@ from ROS.e_nose_classifier.src.e_nose_classification_test import eNoseClassifica
 from ROS.e_nose_classifier.src.e_nose_subscriber import eNoseSubscriber
 from ROS.e_nose_classifier.src.online_reader import OnlineReader
 from classification.lstm_model import SmelLSTM
+from classification.knn import KNN
+
 from e_nose.measurements import DataType
 import numpy as np
 import rospy
@@ -25,18 +27,25 @@ class ClassifierOrganizer:
         self.from_sample = 0
         self.sub.onUpdate += self.gotNewSample
         self.online.invoke_callback += self.gatheredData
-        self.classifier = SmelLSTM(input_shape=(1, 1, num_working_channels), num_classes=6, hidden_dim_simple=12)
+        self.use_neural_network = True
 
-        #self.model_name = "LSTMTrainable_b625122c_11_batch_size=64,dim_hidden=16,lr=0.073956,return_sequences=True_2020-03-04_19-04-41c78mu_or"
-        #self.model_name = 'LSTMTrainable_15750966_1740_batch_size=128,dim_hidden=6,lr=0.004831,return_sequences=True_2020-03-05_08-08-45fs4p25pg'
+        if self.use_neural_network:
+            self.classifier = SmelLSTM(input_shape=(1, 1, num_working_channels), num_classes=6, hidden_dim_simple=12)
 
-        #self.model_name = 'LSTMTrainable_9c63b3de_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.018191,return_sequences=True_2020-03-05_14-41-14slhxixe7'
-        #self.model_name = 'LSTMTrainable_987a098a_3_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.076584,return_sequences=True_2020-03-05_14-39-01_0r6hi60'
-        #self.model_name = 'LSTMTrainable_f9244f1c_1_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.070814,return_sequences=True_2020-03-05_23-30-57teprjxra'
+            #self.model_name = "LSTMTrainable_b625122c_11_batch_size=64,dim_hidden=16,lr=0.073956,return_sequences=True_2020-03-04_19-04-41c78mu_or"
+            #self.model_name = 'LSTMTrainable_15750966_1740_batch_size=128,dim_hidden=6,lr=0.004831,return_sequences=True_2020-03-05_08-08-45fs4p25pg'
 
-        self.model_name = 'LSTMTrainable_231f7674_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=12,lr=0.050039,stateful=True,use_lstm=True_2020-03-06_21-24-4970osptsr'
-        self.classifier.summary()
-        self.classifier.load_weights(self.model_name, checkpoint=160, path='classification/models/models_new_train_data_offset/')
+            #self.model_name = 'LSTMTrainable_9c63b3de_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.018191,return_sequences=True_2020-03-05_14-41-14slhxixe7'
+            #self.model_name = 'LSTMTrainable_987a098a_3_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.076584,return_sequences=True_2020-03-05_14-39-01_0r6hi60'
+            #self.model_name = 'LSTMTrainable_f9244f1c_1_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.070814,return_sequences=True_2020-03-05_23-30-57teprjxra'
+
+            self.model_name = 'LSTMTrainable_231f7674_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=12,lr=0.050039,stateful=True,use_lstm=True_2020-03-06_21-24-4970osptsr'
+            self.classifier.summary()
+            self.classifier.load_weights(self.model_name, checkpoint=160, path='classification/models/models_new_train_data_offset/')
+
+        else:
+            self.datatype = DataType.HIGH_PASS
+            self.classifier = KNN(num_working_channels, data_dir='data', data_type=self.datatype)
         print('ros e_nose classification node started successfully')
 
     def startMeas(self):
@@ -56,7 +65,12 @@ class ClassifierOrganizer:
         data = self.online.get_since_n_as_measurement(self.from_sample)
         print(data.correct_channels)
         print(data.get_data().shape)
-        prediction = self.classifier.predict_live(data)
+        if self.use_neural_network:
+            prediction = self.classifier.predict_live(data)
+        else:
+            data_for_classifier = data.get_data_as(self.datatype)
+            if (data.shape[0] > 40):
+                prediction = self.classifier.predict(data_for_classifier)
         print('prediction: ', prediction)
         self.pub_test.send_classification(prediction)
         self.pub.send_classification(prediction)
