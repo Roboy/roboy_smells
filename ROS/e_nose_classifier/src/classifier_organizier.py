cef7e88c20a569681264fc5b791bc6e00b57c476
missxa <aliona.kharchenko@gmail.com>
2020-03-09 14:54:15 +0100

    bugfixes

--- ROS/e_nose_classifier/src/classifier_organizier.py
+++ ROS/e_nose_classifier/src/classifier_organizier.py
@@ -5,7 +5,7 @@ from ROS.e_nose_classifier.src.online_reader import OnlineReader
 from ROS.e_nose_classifier.src.e_nose_measurement_publisher import eNoseMeasurementPublisher
 from classification.lstm_model import SmelLSTM
 from classification.knn import KNN
-from e_nose.measurements import DataType
+from e_nose.measurements import DataType, StandardizationType
 import time
 import numpy as np
 import rospy
@@ -29,18 +29,16 @@ class ClassifierOrganizer:
         working_channels[failing_channels] = False
         num_working_channels = np.count_nonzero(working_channels)
         print(num_working_channels)
-        self.online = OnlineReader(5, override_working_channels=working_channels)
+        self.online = OnlineReader(5, standardization=StandardizationType.LAST_REFERENCE, override_working_channels=working_channels)
         self.from_sample = 0
         self.sub.onUpdate += self.got_new_sample
         self.online.invoke_callback += self.gathered_data
-        self.use_neural_network = True
+        self.use_neural_network = False
 
         if self.use_neural_network:
             self.classifier = SmelLSTM(input_shape=(1, 1, num_working_channels), num_classes=6, hidden_dim_simple=12)
-
-            # self.model_name = "LSTMTrainable_b625122c_11_batch_size=64,dim_hidden=16,lr=0.073956,return_sequences=True_2020-03-04_19-04-41c78mu_or"
-            # self.model_name = 'LSTMTrainable_15750966_1740_batch_size=128,dim_hidden=6,lr=0.004831,return_sequences=True_2020-03-05_08-08-45fs4p25pg'
-
+# self.model_name = "LSTMTrainable_b625122c_11_batch_size=64,dim_hidden=16,lr=0.073956,return_sequences=True_2020-03-04_19-04-41c78mu_or"
+            # self.model_name = 'LSTMTrainable_15750966_1740_batch_size=128,dim_hidden=6,lr=0.004831,return_sequences=True_2020-03-05_08-08-45fs4p25pg' 
             # self.model_name = 'LSTMTrainable_9c63b3de_15_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.018191,return_sequences=True_2020-03-05_14-41-14slhxixe7'
             # self.model_name = 'LSTMTrainable_987a098a_3_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.076584,return_sequences=True_2020-03-05_14-39-01_0r6hi60'
             # self.model_name = 'LSTMTrainable_f9244f1c_1_batch_size=128,data_preprocessing=high_pass,dim_hidden=6,lr=0.070814,return_sequences=True_2020-03-05_23-30-57teprjxra'
